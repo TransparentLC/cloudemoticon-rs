6755d1e85c40058fa873f67df2bfebb730e34740
✨小透明・宸✨ <47057319+TransparentLC@users.noreply.github.com>
2025-07-11 10:56:05 +0800

    修复Linux下webkit显示空白问题

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // snap - Apps using Webkit, like Foliate, Wike, Caveliar or GNOME Web do not show content Ubuntu 22.04 with NVIDIA graphics - Ask Ubuntu
+    // https://askubuntu.com/questions/1493204#answer-1493214
+    if cfg!(target_os = "linux") {
+        unsafe {
+            std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+        }
+    }
     cloudemoticon_rs_lib::run()
 }
